2838f01166529210c0bf3349dc61761798466fa2
Harshil P <162300039+erzer12@users.noreply.github.com>
2025-06-27 20:33:53 +0530

    Update ProgressIndicator.tsx

--- components/ui/ProgressIndicator.tsx
+++ components/ui/ProgressIndicator.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text, ViewStyle } from 'react-native';
-import { getThemeColors } from '@/constants/Colors';
+import { getThemeColors } from '@/constants/theme';
 import { useTheme } from '@/context/ThemeContext';
 import Animated, { 
   useAnimatedStyle, 
